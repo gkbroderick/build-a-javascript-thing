f537cc444e367d2ebd06438c42a8f24561b98b02
GBroderick <galen.broderick@gmail.com>
2014-12-02 11:09:40 -0800

    Add alert warning and JS Event Listeners

--- Galen-Loann/script_play.js
+++ Galen-Loann/script_play.js
@@ -1,31 +1,76 @@
-// add event listener to submit
-document.getElementById('cartSweater').addEventListener('submit', calcTotal);
-// create function and argument for listener
-function calcTotal(event) {
+var sweaterOne = document.getElementById('SwtOne'),
+sweaterTwo = document.getElementById('SwtTwo'),
+sweaterThree = document.getElementById('SwtThree'),
+sizeOne = document.getElementById('sizeSwtOne'),
+sizeTwo = document.getElementById('sizeSwtTwo'),
+sizeThree = document.getElementById('sizeSwtThree'),
+alert = document.getElementById('alert'),
+numList = ["One", "Two", "Three"];
+
+function submit(event) {
 // so page does not refresh on submit click by default
   event.preventDefault();
+  calcTotal();
+}
+
+function calcTotal() {
 // passing variables inside function, not global
 // using valueAsNumber in place of parseInt to make sure we get number value back
 // http://html5doctor.com/the-output-element/
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
-  var sweaterOne = document.getElementById('SwtOne').valueAsNumber || 0;
-      sweaterTwo = document.getElementById('SwtTwo').valueAsNumber || 0;
-      sweaterThree = document.getElementById('SwtThree').valueAsNumber || 0;
-      sizeOne = document.getElementById('sizeSwtOne').value,
-      sizeTwo = document.getElementById('sizeSwtTwo').value,
-      sizeThree = document.getElementById('sizeSwtThree').value;
-// making sure it works in console
-  // console.log(sweaterOne);
-  // console.log(sweaterTwo);
-  // console.log(sweaterThree);
-  // console.log(sizeOne);
-  // console.log(sizeTwo);
-  // console.log(sizeThree);
-  var subtotal = (sweaterOne * 29.99) + (sweaterTwo * 35.99) + (sweaterThree * 49.99);
+  var sweaterOneVal = sweaterOne.valueAsNumber || 0;
+  var sweaterTwoVal = sweaterTwo.valueAsNumber || 0;
+  var sweaterThreeVal = sweaterThree.valueAsNumber || 0;
+
+  var subtotal = (sweaterOneVal * 29.99) + (sweaterTwoVal * 35.99) + (sweaterThreeVal * 19.99);
   // use toFixed method to pass in 2 decimal places
   var total = '$' + subtotal.toFixed(2);
 
   document.getElementById('textTotal').value = total;
 }
-// still need to validate quantity dropdown!
-// still need to require size input, provide alert if none!
+
+function checkSize(quant, size) {
+  if ((quant > 0) && (size != "")) {
+    alert.classList.remove("color1");
+    alert.textContent = "THANKS";
+  }
+  else if ((quant > 0) && (size == "")){
+    alert.classList.add("color1");
+    alert.textContent = "Please Enter a Size";
+  }
+  else {
+    alert.classList.add("color1");
+    alert.textContent = "Please Enter a Quantity";
+  }
+}
+
+// Event Listeners
+
+document.getElementById('cartSweater').addEventListener('submit', submit);
+
+sweaterOne.addEventListener("change", function(){
+    calcTotal();
+    checkSize(sweaterOne.value, sizeSwtOne.value);
+  },false);
+
+sizeSwtOne.addEventListener("change", function(){
+    checkSize(sweaterOne.value, sizeSwtOne.value);
+  },false);
+
+sweaterTwo.addEventListener("change", function(){
+    calcTotal();
+    checkSize(sweaterTwo.value, sizeSwtTwo.value);
+  },false);
+
+sizeSwtTwo.addEventListener("change", function(){
+    checkSize(sweaterTwo.value, sizeSwtTwo.value);
+  },false);
+
+sweaterThree.addEventListener("change", function(){
+    calcTotal();
+    checkSize(sweaterThree.value, sizeSwtThree.value);
+  },false);
+
+sizeSwtThree.addEventListener("change", function(){
+    checkSize(sweaterThree.value, sizeSwtThree.value);
+  },false);
