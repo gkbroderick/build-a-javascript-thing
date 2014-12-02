ea69baa1cf388ca523a6e88ef19c121f47db075f
GBroderick <galen.broderick@gmail.com>
2014-12-02 12:40:36 -0800

    Fix Index omissions, add back text area 'match' functionality

--- Galen-Loann/js/color.js
+++ Galen-Loann/js/color.js
@@ -1,3 +1,11 @@
+var message = document.getElementById("message"),
+messagePreview = document.getElementById("messagePreview");
+
+
+message.addEventListener("keypress", function(){
+  messagePreview.value = message.value;
+}, false);
+
 // onclick color for text field
 
 document.getElementById("squareOne").onclick = function(){
