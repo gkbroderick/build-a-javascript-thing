c6edfa460cae380c34dad5c7df7a04de2c65ef7c
Loann Lam <lolanysf@gmail.com>
2014-12-02 12:26:32 -0800

    Added message feature: text input field with color options. With corresponding color JS file.

---
+++ Galen-Loann/js/color.js
@@ -0,0 +1,8 @@
+// onclick color for text field
+
+document.getElementById("squareOne").onclick = function(){
+  document.getElementById('messagePreview').className = 'orange';
+}
+document.getElementById("squareTwo").onclick = function(){
+  document.getElementById('messagePreview').className = 'green';
+}
