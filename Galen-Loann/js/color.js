8ae0c3e6ee1a2b84e4af83203c800b7aa21fcf24
Loann Lam <lolanysf@gmail.com>
2014-12-04 11:43:17 -0800

    refactor JS, removed santa from html

--- Galen-Loann/js/color.js
+++ Galen-Loann/js/color.js
@@ -1,53 +1,23 @@
-
-// variable for message container ID
-// variable colors 3 buttons
-// access colors by data attribute: data-color
-// click on square and get attribute color, check if color = color
-// if true then set attribute color in message container to this.attribute
-
 var message = document.getElementById("message"),
 messagePreview = document.getElementById("messagePreview");
-
-
 message.addEventListener("keypress", function(){
   messagePreview.value = message.value;
 }, false);
 
 // onclick color for text field
-
-var preview = document.getElementById('messagePreview');
-var colorMenu = document.getElementById('colorMenu');
-var colors = colorMenu.dataset.color;
-
-document.querySelector('[data-color="red"]').onclick = function() {
-  preview.setAttribute('data-color', 'red');
-}
-
-document.querySelector('[data-color="green"]').onclick = function() {
-  preview.setAttribute('data-color', 'green');
-}
-
-document.querySelector('[data-color="blue"]').onclick = function() {
-  preview.setAttribute('data-color', 'blue');
-}
-
-
-//  event delegation
-
-// var colorMenu = document.querySelectorAll(".colorMenu colorBtn");
-// var createColorHandler = function(button) {
-//   return function() {
-//     if(!button.classList.contains("active"))
-//       button.classList.add("active");
-//     else
-//       button.classList.remove("active");
-//   };
-// };
-
-// for(var i = 0; i < buttons.length; i++) {
-//   buttons[i].addEventListener("click", createToolBarButtonHandler(buttons[i]));
-// }
-
+document.getElementById("colorMenu").addEventListener("click",function(e) {
+  var preview = document.getElementById('messagePreview');
+  var colorCheck = e.target.getAttribute('data-color');
+  if (colorCheck === 'red') {
+    preview.setAttribute('data-color', 'red');
+  }
+  else if (colorCheck === 'green') {
+    preview.setAttribute('data-color', 'green');
+  }
+  else if (colorCheck === 'blue') {
+    preview.setAttribute('data-color', 'blue');
+  }
+});
 
 // create a holiday message, using event fuction
 document.getElementById('message').onkeyup = function() {
